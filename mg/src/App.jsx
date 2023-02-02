55f0369cc30b5f2c5fbd43d4eaf64f9d6ec7e2eb
DeVon DuPree <devondupree@Air-DeVonny-Pooh.local>
2023-02-01 16:21:53 -0800

    first commit

---
+++ mg/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-[100px] text-blue-600">
+      <h1>MG Investments, LLC</h1>
+    </div>
+  )
+}
+
+export default App
